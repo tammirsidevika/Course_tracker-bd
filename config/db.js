5d6a2a2f197f845b454c2b62f0892cebc7a747f7
tammirsidevika <tammirsidevika05@gmail.com>
2026-07-15 22:43:42 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,11 +2,14 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(
+    const conn = await mongoose.connect(
       "mongodb+srv://tammirsidevika05_db_user:<db_password>@cluster0.7ywegpc.mongodb.net/"
     );
 
     console.log("Database Connected Successfully");
+    console.log("Host:", conn.connection.host);
+    console.log("Database:", conn.connection.name);
+    console.log("Ready State:", mongoose.connection.readyState);
   } catch (error) {
     console.log("Database Connection Failed");
     console.log(error);
