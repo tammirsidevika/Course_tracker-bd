575bdbd0845d8947f3f1edcb9052e918d5cd8ecb
tammirsidevika <tammirsidevika05@gmail.com>
2026-07-16 11:19:41 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,19 +1,35 @@
-const mongoose = require("mongoose");
+const express = require("express");
+const cors = require("cors");
+const dns = require("dns");
 
-const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(
-      "mongodb+srv://tammirsidevika05_db_user:<db_password>@cluster0.7ywegpc.mongodb.net/"
-    );
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+const courseRoutes = require("./routes/courseRoutes");
 
-    console.log("Database Connected Successfully");
-    console.log("Host:", conn.connection.host);
-    console.log("Database:", conn.connection.name);
-    console.log("Ready State:", mongoose.connection.readyState);
-  } catch (error) {
-    console.log("Database Connection Failed");
-    console.log(error);
-  }
-};
+const app = express();
 
-module.exports = connectDB;
+// Optional (for MongoDB Atlas)
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Connect Database
+connectDB();
+
+// Routes
+app.use("/api/auth", authRoutes);
+app.use("/api/courses", courseRoutes);
+
+// Test Route
+app.get("/", (req, res) => {
+    res.send("Course Tracker Backend Running...");
+});
+
+// Start Server
+const PORT = 3001;
+
+app.listen(PORT, () => {
+    console.log(`Server Running at http://localhost:${PORT}`);
+});
